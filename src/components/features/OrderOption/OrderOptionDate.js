52ba4570b2e82ad15fd6328e976dd64f658e44bc
Bartosz Łepski <lepski.bartosz@gmail.com>
2020-06-21 14:35:07 +0200

    Add test for all order options

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
+import PropTypes from 'prop-types';
 
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
@@ -7,11 +8,17 @@ class OrderOptionDate extends React.Component {
   state = {
     startDate: new Date(),
   };
+
+  static propTypes = {
+    setOptionValue: PropTypes.func,
+  }
  
   handleChange = date => {
+    const {setOptionValue} = this.props;
     this.setState({
       startDate: date,
     });
+    setOptionValue(date);
   };
  
   render() {
--- src/components/features/OrderOption/OrderOptionText.js
+++ src/components/features/OrderOption/OrderOptionText.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const OrderOptionText = () => (
+const OrderOptionText = ({currentValue, setOptionValue}) => (
   <div>
-    <input type='text'></input>
+    <input 
+      type='text'
+      value={currentValue}
+      onChange={event => setOptionValue(event.currentTarget.value)}
+    >
+
+    </input>
   </div>
 );
 
 OrderOptionText.propTypes = {
   id: PropTypes.any,
+  setOptionValue: PropTypes.func,
+  currentValue: PropTypes.string,
+
 };
 
 export default OrderOptionText;
